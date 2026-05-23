f057959139a4004f116596953644f1eac7fa5392
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2026-05-23 16:29:53 +0200

    Adding the the files of loops demo

---
+++ loop_while.py
@@ -0,0 +1,17 @@
+# While loop = used to repeat a block of code as long as a condition remain 'True'
+#             we re-check the condition at the end of the loop
+
+name = input("Enter your name: ")
+while name == "":
+    name = input("Enter your name: ")
+
+
+age = int(input("Enter your age: "))
+while age < 0:
+    print("Your age must be greater than or equal to zero.")
+    age = int(input("Enter your age: "))
+
+
+print(f"{name} Welcome to Rwanda")
+print(f"You are {age} years old.")
+
