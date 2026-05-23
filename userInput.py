8c06923c30966b43511bb09c72a03e1b2e89536e
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2026-05-23 14:54:48 +0200

    Adding the file demonstrating the user input in python

---
+++ userInput.py
@@ -0,0 +1,18 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print(name)
+print(age)
+
+#For the input users all they are now currently string whenever you want to use them as their nature
+#1
+gpa = float(input("Enter your GPA: "))
+print(gpa)
+gpa = gpa+5
+print(gpa)
+
+#2
+income = input("Enter your income: ")
+print(type(income))
+income = int(income)*0.25
+print(income)
