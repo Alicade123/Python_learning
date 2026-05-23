cf17015a98c387859e429fa4e7e4edd33a879d8e
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2026-05-23 13:45:27 +0200

    adding the file that highlight the basic of python variables

---
+++ variables.py
@@ -0,0 +1,17 @@
+# Variable = A reusable container for a value (String, integer, float, boolean)
+#           The variable behaves as if it was the value that it contains
+
+full_name = "Alicade Abiturije Dusabe"
+age = 24
+gpa = 3.2
+is_student = False
+
+print(f"Hello {full_name}")
+print(f"you are {age} years old")
+print(f"Your gpa is {gpa}")
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are not a student")
+
