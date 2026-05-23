529a79b36d004fe1a7f31812b4e74c1bea995cb3
Alicade Abiturije Dusabe <abiturije1alicade@gmail.com>
2026-05-23 13:36:51 +0200

    Adding the first python program

---
+++ main.py
@@ -0,0 +1,6 @@
+# This is my first python program
+
+print("Hello World")
+print("My name is Alicade")
+print("This is python lesson in cloud engineering program through [TheGym](https://www.the-gym.rw/), [Ojemba](https://en.ojemba.de/) & [MaibornWolff](https://www.maibornwolff.de/en/)")
+print("This is realy amazing!")
